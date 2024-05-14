740e60a860019a68fe8d4ecee2dfffca9d324873
XabierCendon <xcendpazo@ieschandomonte.edu.es>
2024-05-14 19:42:21 +0200

    Algo

--- src/Menu/MainPage_controller.java
+++ src/Menu/MainPage_controller.java
@@ -24,14 +24,19 @@ public class MainPage_controller {
 
     @FXML
     private Button botonSalirJuego;
-    
+
+    @FXML
+    private Button botonEstadisticas;
+
+
 
     @FXML
     void metodoBuscarPartida(ActionEvent event) {
         ObservableList<String> partidas = Menu_modelo.buscarPartidas();
         if (partidas != null) {
             SwitchScene cambiarEscena = new SwitchScene();
-            cambiarEscena.cambiarEscena("../ListaPartidas/listaPartidas_proyecto.fxml", botonBuscarPartida, partidas);
+            cambiarEscena.cambiarEscena("../ListaPartidas/listaPartidas_proyecto.fxml",
+                    botonBuscarPartida, partidas);
         } else {
             System.out.println("Hubo un error al buscar las partidas");
         }
@@ -43,13 +48,13 @@ public class MainPage_controller {
     }
 
     @FXML
-    void metodoEnsenharInfo(ActionEvent event) {    
+    void metodoEnsenharInfo(ActionEvent event) {
         Alert alertaInfo = new Alert(Alert.AlertType.INFORMATION);
         alertaInfo.setTitle("Información");
         alertaInfo.setHeaderText("Piedra, peper, tijera, lagarto, spock");
         alertaInfo.setContentText("Programa hecho por Eloy Rodal Pérez y Xabier Cendón Pazos");
         alertaInfo.showAndWait();
-    }   
+    }
 
     @FXML
     void metodoSalirJuego(ActionEvent event) {
@@ -57,4 +62,8 @@ public class MainPage_controller {
         cambiarEscena.cambiarEscena("../Login/paginaLogin_proyecto.fxml", botonSalirJuego);
     }
 
+    @FXML
+    void metodoMostrarEstadisticas(ActionEvent event) {
+
+    }
 }
