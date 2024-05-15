872e7679af98670fa2f2b61d24185fb1bf833f36
XabierCendon <xcendpazo@ieschandomonte.edu.es>
2024-05-15 21:58:53 +0200

    Jugar partidas funcional

--- src/Util/Util.java
+++ src/Util/Util.java
@@ -127,7 +127,7 @@ public class Util {
 
 
 
-    public static void guardarDatosJugadorGanadorPartidaEnBD(Partida partida, String username2) {
+    public static void guardarDatosJugadorGanadorPartidaEnBD(Partida partida) {
         try {
             Connection conexionBD = ConexionBD.BD.conectarBD();
             // Si la conexión es nula, mostrar un mensaje de error
@@ -138,9 +138,10 @@ public class Util {
                 // Crear un Statement para ejecutar la consulta
                 try (Statement consulta = conexionBD.createStatement()) {
                     // Crear la consulta
-                    if (username2 != null) {
-                        String sql = "UPDATE Partida SET JugadorGanador = '" + username2
-                                + "' WHERE ID = " + partida.getIdPartida();
+                    if (partida.getJugadorGanador() != null) {
+                        String sql = "UPDATE Partida SET JugadorGanador = '"
+                                + partida.getJugadorGanador() + "' WHERE ID = "
+                                + partida.getIdPartida();
                         consulta.executeUpdate(sql);
                     }
                     // Si la consulta devuelve un resultado, el login es correcto
@@ -155,7 +156,6 @@ public class Util {
         }
     }
 
-    // TODO ARREGLAR ESTE MÉTODO
     public static void actualizarPartidasGanadasYJugadas(Partida partida) {
         try {
             Connection conexionBD = ConexionBD.BD.conectarBD();
@@ -166,6 +166,39 @@ public class Util {
             } else {
                 // Crear un Statement para ejecutar la consulta
                 try (Statement consulta = conexionBD.createStatement()) {
+
+
+
+                    // Imprimir por consola el valor de las partidas jugadas y ganadas de los
+                    // jugadores antes de actualizar
+                    System.out.println("Partidas jugadas antes de actualizar: ");
+                    String sqlJugador1Antes =
+                            "SELECT GamesPlayed, GamesWon FROM Jugador WHERE Username = '"
+                                    + partida.getJugador1() + "'";
+                    consulta.executeQuery(sqlJugador1Antes);
+                    if (consulta.getResultSet().next()) {
+                        System.out.println(
+                                "Jugador 1: " + partida.getJugador1() + " - Partidas jugadas: "
+                                        + consulta.getResultSet().getInt("GamesPlayed")
+                                        + " - Partidas ganadas: "
+                                        + consulta.getResultSet().getInt("GamesWon"));
+                    }
+
+                    // Lo mismo para el jugador 2
+                    String sqlJugador2Antes =
+                            "SELECT GamesPlayed, GamesWon FROM Jugador WHERE Username = '"
+                                    + partida.getJugador2() + "'";
+                    consulta.executeQuery(sqlJugador2Antes);
+                    if (consulta.getResultSet().next()) {
+                        System.out.println(
+                                "Jugador 2: " + partida.getJugador2() + " - Partidas jugadas: "
+                                        + consulta.getResultSet().getInt("GamesPlayed")
+                                        + " - Partidas ganadas: "
+                                        + consulta.getResultSet().getInt("GamesWon"));
+                    }
+
+
+
                     // Actualizar las partidas jugadas de los jugadores
                     String sqlJugador1 =
                             "UPDATE Jugador SET GamesPlayed = GamesPlayed + 1 WHERE Username = '"
@@ -184,9 +217,42 @@ public class Util {
                                         + partida.getJugadorGanador() + "'";
                         consulta.executeUpdate(sqlGanador);
                     }
+
+
+
+                    // Imprimir por consola el valor de las partidas jugadas y ganadas de los
+                    // jugadores después de actualizar
+                    System.out.println("Partidas jugadas después de actualizar: ");
+                    String sqlJugador1Despues =
+                            "SELECT GamesPlayed, GamesWon FROM Jugador WHERE Username = '"
+                                    + partida.getJugador1() + "'";
+                    consulta.executeQuery(sqlJugador1Despues);
+                    if (consulta.getResultSet().next()) {
+                        System.out.println(
+                                "Jugador 1: " + partida.getJugador1() + " - Partidas jugadas: "
+                                        + consulta.getResultSet().getInt("GamesPlayed")
+                                        + " - Partidas ganadas: "
+                                        + consulta.getResultSet().getInt("GamesWon"));
+                    }
+                    // Lo mismo para el jugador 2
+                    String sqlJugador2Despues =
+                            "SELECT GamesPlayed, GamesWon FROM Jugador WHERE Username = '"
+                                    + partida.getJugador2() + "'";
+                    consulta.executeQuery(sqlJugador2Despues);
+                    if (consulta.getResultSet().next()) {
+                        System.out.println(
+                                "Jugador 2: " + partida.getJugador2() + " - Partidas jugadas: "
+                                        + consulta.getResultSet().getInt("GamesPlayed")
+                                        + " - Partidas ganadas: "
+                                        + consulta.getResultSet().getInt("GamesWon"));
+                    }
+
+
+
                     // Si la consulta devuelve un resultado, el login es correcto
                 } catch (SQLException e1) {
                     System.out.println("Error al ejecutar la consulta");
+                    e1.printStackTrace();
                 }
             }
             // Cerrar la conexión con la BD
